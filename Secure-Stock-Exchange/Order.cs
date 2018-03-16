6dbdfb43843e6041d29b10fb2d33f85b7f1619fd
Connor McCauley <cdmc13@outlook.com>
2018-03-16 17:36:32 -0400

    Add basic class data for Order, Company and first look at interconnectivity in accord with class diagram.

---
+++ Secure-Stock-Exchange/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secure_Stock_Exchange
+{
+    public enum orderType { Buy, Sell };
+
+    class Order
+    {
+        private orderType _orderType;
+        public DateTime orderDateTime;
+        public int orderSize;
+        private double _orderPrice;
+
+        void setPrice(double price)
+        {
+            this._orderPrice = price;
+        }
+
+        double getPrice()
+        {
+            return this._orderPrice;
+        }
+
+        void setType(orderType ot)
+        {
+            this._orderType = ot;
+        }
+
+        orderType getType()
+        {
+            return this._orderType;
+        }
+    }
+}
