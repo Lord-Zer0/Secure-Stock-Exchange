adbcecde7ae74325f1157378a3539af84bff1ff7
Connor McCauley <cdmc13@outlook.com>
2018-03-15 22:49:19 -0400

    Setup of Data Grids and StockStateSummary

--- Secure-Stock-Exchange/MarketByOrder.Designer.cs
+++ Secure-Stock-Exchange/MarketByOrder.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MarketByOrder));
             this.SuspendLayout();
             // 
             // MarketByOrder
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 864);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MarketByOrder";
             this.Text = "Market Depth By Price (<<COMPANY>>)";
             this.ResumeLayout(false);
--- Secure-Stock-Exchange/MarketByPrice.Designer.cs
+++ Secure-Stock-Exchange/MarketByPrice.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PriceDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.PriceDataGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // PriceDataGrid
+            // 
+            this.PriceDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PriceDataGrid.Location = new System.Drawing.Point(21, 23);
+            this.PriceDataGrid.Name = "PriceDataGrid";
+            this.PriceDataGrid.RowTemplate.Height = 40;
+            this.PriceDataGrid.Size = new System.Drawing.Size(896, 816);
+            this.PriceDataGrid.TabIndex = 0;
             // 
             // MarketByPrice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(943, 865);
+            this.Controls.Add(this.PriceDataGrid);
             this.Name = "MarketByPrice";
             this.Text = "Market Depth By Price (<<COMPANY>>)";
+            ((System.ComponentModel.ISupportInitialize)(this.PriceDataGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView PriceDataGrid;
     }
 }
